3acc1dc99db1221594dc956a1ebf0d67d2d11e70
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-10-13 20:56:10 +0900

    구슬치기 시뮬레이션

---
+++ 과제/실습과제(3) 구슬치기 시뮬레이션/Child.h
@@ -0,0 +1,19 @@
+//컴퓨터공학전공 20200675 문서연
+#ifndef _Child_h
+#define _Child_h
+
+class Child {
+	int bead;
+
+public:
+	Child(void);
+	Child(int bead);
+public:
+	inline int getBead(void) const { return bead; };
+	bool setBead(int bead);
+	void showInfo(void) const;
+	void win(Child *loser, int bead);
+	void lose(Child *winner, int bead);
+};
+
+#endif // !_Child_h
