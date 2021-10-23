39bdff4bec90450b09fc895e989dc58ed792871c
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-10-23 18:54:16 +0900

    정수의동적배열

--- Exercise_forMidExam2/교재 연습/정수의 동적배열/main.cpp
+++ Exercise_forMidExam2/교재 연습/정수의 동적배열/main.cpp
@@ -2,21 +2,30 @@
 
 using namespace std;
 
-int main(void)
+int sums(int numbers[], int size) {
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += numbers[i];
+	}
+	return sum;
+}
+void sets(int numbers[], int size)
 {
-	int* numbers = new int[5];
 	int num;
-
 	cout << "정수 5개 입력>> ";
 	for (int i = 0; i < 5; i++) {
 		cin >> num;
 		numbers[i] = num;
 	}
 
-	int sum = 0;
-	for (int i = 0; i < 5; i++) {
-		sum += numbers[i];
-	}
+}
+int main(void)
+{
+	int* numbers = new int[5];
+	int size = 5;
+
+	sets(numbers, size);
+	int sum = sums(numbers, size);
 	cout << "평균 " << (sum / (double)5);
 	return 0;
 }
